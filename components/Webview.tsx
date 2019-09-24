437bbaeb96987c382c49e09152497d13d04d6398
ShunOmine <shun12baseball@gmail.com>
2019-09-24 11:45:02 +0900

    fix

--- components/Webview.tsx
+++ components/Webview.tsx
@@ -3,8 +3,14 @@ import WebView from "react-native-webview"
 
 interface Props {
   uri: string
+  navigation: any,
 }
 export default class Webview extends Component<Props>{
+  componentWillMount(): void {
+   this.props.navigation.setParams({
+     'title': this.props.uri
+   })
+  }
   render() {
     const { uri } = this.props
     return (
